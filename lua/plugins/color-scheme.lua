68c0051df489945944378c2adfca0de6db2c05d1
huydq189 <salt.coffee189@gmail.com>
2024-11-03 20:41:58 +0700

    update lsp, lazygit and color scheme

---
+++ lua/plugins/color-scheme.lua
@@ -0,0 +1,10 @@
+return {
+  -- add gruvbox
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
