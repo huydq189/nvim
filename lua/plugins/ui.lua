e0edbafbaf7ebc254e69c549f559f84942a4374e
Huy Đỗ <huydo@Huys-Mac-mini.local>
2025-04-29 19:53:59 +0700

    feat(nvim): add vim-tmux-nagivator

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -8,21 +8,11 @@ return {
   {
     "akinsho/bufferline.nvim",
     event = "VeryLazy",
-    keys = {
-      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
-      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
-    },
     opts = {
       options = {
         show_buffer_close_icons = false,
         show_close_icon = false,
       },
-    },
-  },
-  {
-    "folke/snacks.nvim",
-    opts = {
-      scroll = false,
     },
   },
 }
